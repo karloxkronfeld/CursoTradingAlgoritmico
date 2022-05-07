e54a7dc6f5b7f8d559a083ccd80eef5f89667411
karloxkronfeld <kmorelo@gmail.com>
2022-05-07 12:06:34 -0500

    commit

--- clase2.py
+++ clase2.py
@@ -1,21 +1,34 @@
 import MetaTrader5 as mt5
 
-mt5.initialize()
 
+
+##EJERCICIO BUY
+mt5.initialize()
 request = {
         "action":mt5.TRADE_ACTION_DEAL,
         "symbol":'BTCUSD',
-        "type" : mt5.ORDER_TYPE_BUY,
-        "price": mt5.symbol_info_tick('BTCUSD').ask,
+        "type" : mt5.ORDER_TYPE_BUY, ####COMPRA
+        "price": mt5.symbol_info_tick('BTCUSD').ask, #PRECIO ASK
         "volume":0.01,
-        "comment":'NOMBRE',
+        "comment":'NOMBRE______',
         "type_filling":mt5.ORDER_FILLING_IOC
 }
 
 result = mt5.order_send(request)
-print("Orden enviada en {}".format("BTCUSD"));
-if result.retcode != mt5.TRADE_RETCODE_DONE:
-    print("ERROR: {} ".format(result.comment))
+# print("Orden enviada en {}".format("BTCUSD"));
+# if result.retcode != mt5.TRADE_RETCODE_DONE:
+#     print("ERROR: {} ".format(result.comment))
 
-    # mt5.shutdown()
+###EJERCICIO BUY-LIMIT
+mt5.initialize()
+request = {
+        "action":mt5.TRADE_ACTION_PENDING,
+        "symbol":'BTCUSD',
+        "type" : mt5.ORDER_TYPE_BUY_LIMIT, ####COMPRA
+        "price": mt5.symbol_info_tick('BTCUSD').ask+200, #PRECIO ASK
+        "volume":0.01,
+        "comment":'NOMBRE_____',
+        "type_filling":mt5.ORDER_FILLING_IOC
+}
 
+mt5.order_send(request)
